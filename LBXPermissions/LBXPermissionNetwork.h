7b8ff51a9f181dee8e65a096d643d1351d8c0548
lbx <lbxia20091227@foxmail.com>
2017-12-07 20:13:04 +0800

    update

---
+++ LBXPermissions/LBXPermissionNetwork.h
@@ -0,0 +1,16 @@
+//
+//  LBXPermissionNetwork.h
+//  Demo
+//
+//  Created by lbx on 2017/12/7.
+//  Copyright © 2017年 lbx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LBXPermissionNetwork : NSObject
+
++ (void)authorizeWithCompletion:(void(^)(BOOL granted,BOOL firstTime))completion;
+
+
+@end
